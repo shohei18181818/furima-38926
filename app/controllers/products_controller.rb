3e32ea8a47d3bd764669743ed788894162b57086
shohei18181818 <hob2509@yahoo.co.jp>
2023-03-15 20:57:03 +0900

    商品情報編集機能３

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :create]
 
   def index
     @products = Product.order("created_at DESC")
@@ -20,18 +21,16 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    
   end
 
   def edit
-    @product = Product.find(params[:id])
     unless current_user == @product.user
       redirect_to action: :index
     end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -43,5 +42,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:image, :name, :explanation, :category_id, :situation_id, :shipping_charge_id, :prefecture_id, :number_of_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
