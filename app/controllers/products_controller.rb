192df5b3e9e5c7ef12afc7ceb9bd5c7d26f939de
shohei18181818 <hob2509@yahoo.co.jp>
2023-03-15 18:56:36 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,26 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    unless current_user == @product.user
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
